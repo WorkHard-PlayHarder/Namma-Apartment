5ba2ca997125585e5753360ca9831af001629b93
unknown <159817@L9501276.tms.toyota.com>
2015-03-11 00:53:21 -0700

    updatd grunt file

--- app/grunt.js
+++ app/grunt.js
@@ -5,7 +5,7 @@ module.exports = function(grunt){
 	grunt.initConfig({		
 		meta:{
 			//add this header to all the files when minified
-			banner: "Namma apartment project, created at <%= grunt.template.today() %>"
+			banner: "/* Namma apartment project, created at <%= grunt.template.today() %> */"
 		},
 		min{
 			//for the time being minify only the security related files
